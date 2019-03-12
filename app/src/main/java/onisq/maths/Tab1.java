72c92eb238e3fc059e7615b4a5d37d6e1836e017
FeDDoSDudos <frolov.fiodor2015@yandex.ru>
2019-03-12 20:30:23 +0300

    First Commit

---
+++ app/src/main/java/onisq/maths/Tab1.java
@@ -0,0 +1,19 @@
+package onisq.maths;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+
+public class Tab1 extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+
+        return rootView;
+    }
+}
